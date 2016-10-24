2ea7a7d4ce889a7da8d8ad7ef16b909688a48d44
OLDU <oldu@stageconcept.ch>
2016-10-24 11:03:30 +0200

    db test 1

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -10,12 +10,12 @@ public class Main {
         System.out.println("Hello, world!");
         System.out.println();
 
-        // DB
+        // mysql1
         String DB1_DRIVER = "com.mysql.jdbc.Driver";
-        String DB1_URL = "jdbc:mysql://192.168.1.147";
+        String DB1_URL = "jdbc:mysql://192.168.1.114";
         String DB1_USER = "oldu";
         String DB1_PASS = "oldu";
-        String DB1_SQL = "SELECT * from dbtest001.tabletest001";
+        String DB1_SQL = "SELECT * from dbtest.tabletest";
         //String DB1_SQL = "show databases";
         //String DB1_SQL = "show tables from test001";
 
@@ -24,32 +24,35 @@ public class Main {
         dbConnect1.systemOutResultSetInfo();
         dbConnect1.systemOutResultSetContent();
 
-        // DB
+        // #################################################################################
+
+        // mysql2
         String DB2_DRIVER = "com.mysql.jdbc.Driver";
-        String DB2_URL = "jdbc:mysql://192.168.1.149";
+        String DB2_URL = "jdbc:mysql://192.168.1.115";
         String DB2_USER = "oldu";
         String DB2_PASS = "oldu";
-        String DB2_SQL = "SELECT * from dbtest002.tabletest002";
-        //String DB2_SQL = "show databases";
-        //String DB2_SQL = "show tables from dbtest002";
+        String DB2_SQL = "SELECT * from dbtest.tabletest";
 
         DbConnect dbConnect2 = new DbConnect(DB2_DRIVER, DB2_URL, DB2_USER, DB2_PASS);
         dbConnect2.executeQuery(DB2_SQL);
         dbConnect2.systemOutResultSetInfo();
         dbConnect2.systemOutResultSetContent();
 
-        // DB
+        // #################################################################################
+/*
+        // postgresql
         String DB3_DRIVER = "org.postgresql.Driver";
         String DB3_URL = "jdbc:postgresql://192.168.1.139/test1";
         String DB3_USER = "test1";
         String DB3_PASS = "test1";
         String DB3_SQL = "SELECT * FROM public.playground";
-        //String DB3_SQL = "show databases";
-        //String DB3_SQL = "show tables from dbtest002";
 
         DbConnect dbConnect3 = new DbConnect(DB3_DRIVER, DB3_URL, DB3_USER, DB3_PASS);
         dbConnect3.executeQuery(DB3_SQL);
         dbConnect3.systemOutResultSetInfo();
         dbConnect3.systemOutResultSetContent();
+
+        // #################################################################################
+        */
     }
 }
